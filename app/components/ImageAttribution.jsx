a3474e3b8ef2cc73a6b3a34cba3bc9c14b11d252
felixtan <tanf91@gmail.com>
2016-11-17 00:14:29 -0500

    added links to map attribution

--- app/components/ImageAttribution.jsx
+++ app/components/ImageAttribution.jsx
@@ -2,15 +2,7 @@ import React from 'react'
 import partyCodeToName from '../../server/data/partyCodeToName'
 import partyCodeToColor from '../fixtures/partyColors'
 import countryCodeToNames from '../fixtures/countryISOA2toNames'
-
-const licenseLinks = {
-  "CC BY 4.0": "https://creativecommons.org/licenses/by/4.0/",
-  "CC BY 3.0": "https://creativecommons.org/licenses/by/3.0/",
-  "CC BY 2.0": "https://creativecommons.org/licenses/by/2.0/",
-  "CC BY-SA 2.0": "https://creativecommons.org/licenses/by-sa/2.0/",
-  "CC BY-SA 3.0": "https://creativecommons.org/licenses/by-sa/3.0/",
-  "CC BY-SA 4.0": "https://creativecommons.org/licenses/by-sa/4.0/"
-}
+import licenseLinks from '../fixtures/licenseLinks'
 
 const attribStyle = {
   fontSize: '8px'
--- app/containers/BaseMap.jsx
+++ app/containers/BaseMap.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
-// import { render } from 'react-dom'
 import { TileLayer } from 'react-leaflet'
 import basemap from '../config/mapbox.json'
+import licenseLinks from '../fixtures/licenseLinks'
 
 export default function BaseMapLayer(props, context) {
+
+  const openStreetMapLink = "https://www.openstreetmap.org/"
+  const licenseLink = licenseLinks["CC BY-SA 2.0"]
+  const mapboxLink = "https://www.mapbox.com/"
+  const attrib = `Map data © <a href=${openStreetMapLink} target="_blank">OpenStreetMap</a> contributors, <a href=${licenseLink} target="_blank">CC-BY-SA</a>, Imagery © <a href=${mapboxLink} target="_blank">Mapbox</a>`
+
   return (
     <TileLayer url={`${basemap.url}?access_token=${basemap.APIkey}`}
                id={basemap.id}
-               attribution={basemap.attribution}/>
+               attribution={attrib}/>
   )
 }
