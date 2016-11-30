60ca84037ae085f73af2a644c0b2da5a42aad1ed
felixtan <tanf91@gmail.com>
2016-11-30 00:48:44 -0500

    changed path for getting static files in server

--- app/server/index.js
+++ app/server/index.js
@@ -5,18 +5,19 @@ const favicon = require('serve-favicon')
 
 // const INDEX = "/Users/photon/Development/politics-map/client-dev/app/index.html"
 const PORT = process.env.PORT || 3000
+const BUILD = __dirname + "/../../build/"
 const app = express()
 
 app.use(compression())
-app.use(express.static(__dirname))
-app.use(favicon(__dirname + "/../../build/img/favicon/favicon.ico"))
+app.use(express.static(BUILD))
+app.use(favicon(BUILD + "img/favicon/favicon.ico"))
 
 // On relative paths and __dirname:
 // https://stackoverflow.com/questions/20322480/express-js-static-relative-parent-directory
 app.get('/', (req, res) => {
   // res.sendFile(INDEX)
   res.sendFile('index.html', {
-    root: __dirname + '/../../build/'
+    root: BUILD
   }, (err) => {
     if (err) {
       console.log(err)
